3c7b4fbe604b4bc1d5b2ba94760b5780edfd6c4b
Aiden Cullo <aidencullo@gmail.com>
2024-09-17 14:50:03 -0400

    stylize the search bar

--- my-react-app/src/components/SearchBar.js
+++ my-react-app/src/components/SearchBar.js
@@ -1,5 +1,30 @@
+import styled from "styled-components"
+
+const SearchBarContainer = styled.div`
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	width: 100%;
+	height: 50px;
+	background-color: #f2f2f2;
+	border-radius: 5px;
+	margin: 10px 0;
+`;
+
+const SearchBarInput = styled.input`
+	width: 100%;
+	height: 100%;
+	border: none;
+	outline: none;
+	background-color: transparent;
+	font-size: 1.2rem;
+	padding: 0 10px;
+`;
+
 export default function SearchBar() {
   return (
-    <h1>SearchBar</h1>
-  );
+      <SearchBarContainer>
+      <SearchBarInput type="text" placeholder="Search for a movie..." />
+      </SearchBarContainer>
+  )
 }
